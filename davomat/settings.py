f79682510b586e6635e126805d95a38d9fccf574
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2023-03-29 07:18:08 +0500

    'final'

--- davomat/settings.py
+++ davomat/settings.py
@@ -131,12 +131,12 @@ WSGI_APPLICATION = 'davomat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
